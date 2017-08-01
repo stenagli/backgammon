344097c93f4262103516a856304f47041e222cb6
Steven Tenaglia <stenten@gmail.com>
2017-08-01 14:48:24 -0400

    Updated routes to add games@create and api/v1/games#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "games#show"
-  resources :games, only: [:show]
+  resources :games, only: [:show, :create]
+
+  namespace :api do
+    namespace :v1 do
+      resources :games, only: [:show]
+    end
+  end
 end
