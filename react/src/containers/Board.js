f80754edf6fbfed4e61a69e2b434bf62374a953f
Steven Tenaglia <stenten@gmail.com>
2017-08-01 14:34:01 -0400

    Initial game setup in Board.js

--- react/src/containers/Board.js
+++ react/src/containers/Board.js
@@ -4,7 +4,24 @@ class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      white: true,
+      board: []
+      die1: null,
+      die2: null
     }
+  }
+
+  componentDidMount() {
+    let newBoard = new Array(24);
+    newBoard[0] = -2;
+    newBoard[5] = 5;
+    newBoard[7] = 3;
+    newBoard[11] = -5;
+    newBoard[12] = 5;
+    newBoard[16] = -3;
+    newBoard[18] = -5;
+    newBoard[23] = 2;
+
   }
 
   render() {
