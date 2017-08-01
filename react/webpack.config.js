ac8b499890acd1c0b618851ed32938d32b8abb2f
Steven Tenaglia <stenten@gmail.com>
2017-08-01 14:03:04 -0400

    Rendered React App on games#show

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './react/src/main.js',
   output: {
     path: path.resolve(__dirname, '../app/assets/javascripts'),
     filename: 'bundle.js'
