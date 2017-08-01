c0f693d6b9223a8fed024cdf926580a9c0c4e549
Steven Tenaglia <stenten@gmail.com>
2017-08-01 15:20:54 -0400

    Added newBoard() function to Board container

--- react/src/containers/Board.js
+++ react/src/containers/Board.js
@@ -11,7 +11,7 @@ class Board extends React.Component {
     }
   }
 
-  componentDidMount() {
+  newBoard() {
     let newBoard = new Array(24);
     newBoard[0] = -2;
     newBoard[5] = 5;
@@ -21,7 +21,28 @@ class Board extends React.Component {
     newBoard[16] = -3;
     newBoard[18] = -5;
     newBoard[23] = 2;
+    die1 = Math.floor(Math.random() * 6) + 1
+    die2 = Math.floor(Math.random() * 6) + 1
+    // Sort the dice
+    if(die1 > die2) {
+      temp = die1;
+      die1 = die2;
+      die2 = temp;
+    }
+    this.setState( {
+      board: newBoard,
+      die1: die1,
+      die2: die2
+    } );
 
+  }
+
+  componentDidMount() {
+    if(this.props.new)
+      newBoard();
+    else {
+      //fetch(`/api/v1/games/${this.props.gameId}`)
+    }
   }
 
   render() {
