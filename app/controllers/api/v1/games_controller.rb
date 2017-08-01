7565723c37edb6bcb3ea0b4320881965b648d558
Steven Tenaglia <stenten@gmail.com>
2017-08-01 14:46:17 -0400

    Created games_controller for pages and apis

---
+++ app/controllers/api/v1/games_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::GamesController < ApplicationController
+
+  def show
+    #Retrieve a Game from the DB and send it in the response object
+  end
+
+end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,13 @@
 class GamesController < ApplicationController
 
   def show
+    # Renders a React component for the game
 
+  end
+
+  def create
+    # Creates a new Game, saves it in the DB, and renders the Show page for that game.
+    # The show page then issues a fetch request to api/v1/games#show to retrieve the new game
   end
 
 end
