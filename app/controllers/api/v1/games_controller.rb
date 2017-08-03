0e25b537b3e934ef125f4c07b29f9d1e8cd147e8
Steven Tenaglia <stenten@gmail.com>
2017-08-03 11:05:25 -0400

    Added create and update endpoints to games api

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -4,4 +4,12 @@ class Api::V1::GamesController < ApplicationController
     #Retrieve a Game from the DB and send it in the response object
   end
 
+  def create
+    # Create a new game, determine who is white and who is black, and return that and dice roll to clients
+  end
+
+  def update
+    # Update the game with id params[:id] and rebroadcast
+  end
+
 end
