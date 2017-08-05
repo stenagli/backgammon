e8e3214fc5de91e03a9be2127b535b377cfd9929
Steven Tenaglia <stenten@gmail.com>
2017-08-05 15:43:04 -0400

    Added stream_from and receive(data) to game_channel

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,9 +1,14 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_from "game_#{params[:game_id]}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive(data)
+    ActionCable.server.broadcast("game_#{params[:game_id]}", data)
   end
 end
